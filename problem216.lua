81581cc5c63cf70f15a8845486e3af0cc19d482f
Gaichu <gaichu@server.fake>
2014-01-25 11:20:10 +0100

    added require line

--- problem216.lua
+++ problem216.lua
@@ -1,10 +1,13 @@
 -- problem216.lua you might consider not using this as it takes hours to run through 
+
+require 'problem7' -- isPrimeNew(p)
+
 local t,n = {},50*10^6
-  for i=2,n do
-    if isPrimeNew(2*i^2 -1) then
-      if ((((2*i^2 - 1)^0.5)%1) ~= 0) then
-        t[#t+1] = 2*i^2 - 1 
-      end
+for i=2,n do
+  if isPrimeNew(2*i^2 -1) then
+    if ((((2*i^2 - 1)^0.5)%1) ~= 0) then
+      t[#t+1] = 2*i^2 - 1 
     end
   end
-  for i=1,#t do print(i,t[i]) end
+end
+for i=1,#t do print(i,t[i]) end
